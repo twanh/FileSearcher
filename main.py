883ee352c0820472a823b49af32441a89816ea76
Twan Huiskens <huiskenstwan@gmail.com>
2020-06-23 10:32:55 +0200

    Setup basic SearchApp class

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import eel
 
-def startup():
-    eel.init('src', [".js", ".jsx", ".html"])
-    eel.start({'port': 3000}, host='localhost', port="3020", size=(1280, 800))
+from typing import Tuple
 
-if __name__ == '__main__':
-    startup()
-    
+# Type defs
+StartSize = Tuple[int, int]
+StartPos = Tuple[int, int]
+
+
+class SearchApp:
+    def __init__(self, host: str, port: str, start_size: StartSize, start_pos: StartPos):
+        self.host = host
+        self.port = port
+        self.startSize = start_size
+        self.startPosition = start_pos
+        eel.init('src', [".js", ".jsx", ".html"])
+
+    def start(self) -> None:
+        eel.start({'port': 3000}, host=self.host, port=self.port, size=self.startSize)
+
+if __name__ == "__main__":
+    app = SearchApp('localhost', "3020", (1280, 800), (0,0))
+    app.start()
