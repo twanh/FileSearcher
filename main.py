192726b6d775d2dcfa02517b7db0e3d31bc8d1fd
Twan Huiskens <huiskenstwan@gmail.com>
2020-06-25 10:27:17 +0200

    Start working on the connection to the interface

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import eel
+from typing import List, Tuple
 
-from typing import Tuple
+from searcher import Searcher
 
 # Type defs
 StartSize = Tuple[int, int]
 StartPos = Tuple[int, int]
+
+SearchFileRet = List[dict]
+
+# Structire
+# [{name: ..., path: ..., type: ...}]
+
+s = Searcher('D:\GDrive\School 19_20')
+
+@eel.expose("search_file")
+def search_file(filename: str) -> str:
+    print('Searching for files')
+    res = s.basic_search(filename)
+    print(res)
+    return res
+    
 
 
 class SearchApp:
@@ -16,8 +32,16 @@ class SearchApp:
         eel.init('src', [".js", ".jsx", ".html"])
 
     def start(self) -> None:
-        eel.start({'port': 3000}, host=self.host, port=self.port, size=self.startSize)
+        eel.start({'port': 3000}, host=self.host, port=self.port, size=self.startSize, blocking=False)
+
 
 if __name__ == "__main__":
-    app = SearchApp('localhost', "3020", (1280, 800), (0,0))
-    app.start()
+    # app = SearchApp('localhost', "3020", (1280, 800), (0, 0))
+    # app.start()
+    print('Setup')
+    eel.init('web\src', [".js", ".jsx", ".html"])
+    eel.start({'port': 3000}, host='localhost', port="3020", size=(350, 700), block=False)
+    print('Started...')
+    eel.spawn(search_file, 'test')
+    while True:
+        eel.sleep(1.0)
