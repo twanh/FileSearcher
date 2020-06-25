3495a3db10029b0776009c0b59a6600bcf9a3b4b
Twan Huiskens <huiskenstwan@gmail.com>
2020-06-25 17:14:16 +0200

    feat: Create the functionality to run in the background and respond to a hotkey - closes #8

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import eel
 from typing import List, Tuple
 import subprocess, os, platform
 
+import keyboard
+
 from searcher import FileSearchEngine
 from utils import sortByFolder
 
@@ -27,16 +29,38 @@ class SearchApp:
         self.startPosition = start_pos
         self.root_dir = root_dir
         self.file_search = FileSearchEngine(root_dir)
+
+        # The hotkey to use when opening from the background
+        self.hotkey = 'ctrl+shift+space'
+        self.hotkey_callback = None
+
+        # Initialize the app
         eel.init('web\src', [".js", ".jsx", ".html"])
 
+    def show(self):
+        """ Show is triggered when the hotkey is pressed so it unbinds the hotkey and shows"""
+        # Unbind the hotkey to avoid conflicts
+        keyboard.remove_hotkey(self.hotkey_callback)
+        # Show the app
+        eel.show({'port': 3000})
+        
     def start(self) -> None:
-        eel.start({'port': 3000}, host=self.host, port=self.port, size=self.startSize, blocking=False)
+        """ Start the app, show the window and create all the important bindings. """
+        eel.start({'port': 3000}, host=self.host, port=self.port, size=self.startSize, blocking=False, close_callback=lambda p,s: self.close_callback(p,s))
+
+    def start_background_process(self) -> None:
+        """ Starts a background process in which we wait for the hotkey to be pressed. """
+        self.hotkey_callback = keyboard.add_hotkey(self.hotkey, lambda: self.show(), suppress=True)
+
+    def close_callback(self, page, sockets):
+        """ Runs the background process when the main window is closed """
+        self.start_background_process()
 
     @staticmethod
     @eel.expose("search_file")
     def search_file(search_query: str) -> List[dict]:
         """Search file is exposed to the interface and uses FileSearchEngine to search for the given query
-
+ 
         Args:
             search_query (str): The query to search for
         Returns:
@@ -71,7 +95,7 @@ class SearchApp:
 if __name__ == "__main__":
     # Create the search app
     app = SearchApp('localhost', "3020", (1000, 800), (0, 0), "D:\GDrive\School 19_20")
-    # Start the app
+    # Start the app 
     app.start()
     # Loop so the app does not autoclose
     while True:
