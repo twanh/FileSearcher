b5e8e6bd7fa4316b5f9ba63137de214d8862ac9c
Twan Huiskens <huiskenstwan@gmail.com>
2020-06-25 10:37:50 +0200

    Extract the filetype functionality into utils, this allows the program to be a bit more structured

--- searcher.py
+++ searcher.py
@@ -3,30 +3,6 @@ from dataclasses import dataclass
 from typing import List, Dict
 import glob
 
-
-class FileTypes:
-    # File extensions
-    document_ext = ['docx', 'doc', 'odt', 'pdf', 'gdoc', 'xls', 'xlsx', 'ods', 'ppt', 'pptx', 'md', 'gslides', 'gsheets']
-    image_ext = ['gif', 'png', 'jpg', 'jpeg', 'webp', 'tiff', 'psd', 'ai']
-
-    # Names
-    directory = 'dir'
-    document = 'doc'
-    image = 'img'
-    other = 'any'
-
-    def get_file_type(self, path: str) -> str:
-        if os.path.isdir(path): 
-            return self.directory
-        filename, ext = os.path.splitext(path)
-        if ext in self.document_ext:
-            return self.document
-        if ext in self.image_ext:
-            return self.image
-        return self.other
-
-
-
     
 @dataclass
 class SearchResult:
@@ -35,8 +11,8 @@ class SearchResult:
     file_type: str
 
 
-class Searcher:
-    """ Searcher search the given root directory for matches with the given query. """
+class SearchEngine:
+    """ Search engine will index, and allow searching in the root_dir. """
     
     def __init__(self, root_dir: str):
         self.root_dir = root_dir
@@ -44,7 +20,7 @@ class Searcher:
 
     def basic_search(self, query: str) -> List[Dict]:
 
-        
+        res = glob.glob(self.root_dir + '/**/*')
 
 
         return res
---
+++ utils.py
@@ -0,0 +1,28 @@
+import os
+
+# Keeps track of which type of extension should be classified as what
+fileTypes = {
+    'document_ext': ['docx', 'doc', 'odt', 'pdf', 'gdoc', 'xls', 'xlsx', 'ods', 'ppt', 'pptx', 'md', 'gslides', 'gsheets'],
+    # Image extensions
+    'image_ext': ['gif', 'png', 'jpg', 'jpeg', 'webp', 'tiff', 'psd', 'ai'],
+
+}
+
+# Stores the strings used to keep track of the file type.
+fileTypeNames = {
+    'directory':'dir',
+    'document': 'doc',
+    'image': 'img',
+    'other': 'any',
+}
+
+# Returns the type of file (from the fileTypeNames dict) based on the file types stored in fileTypes
+def getFileType(path: str) -> str:
+    if os.path.isdir(path): 
+        return fileTypeNames['directory']
+    filename, ext = os.path.splitext(path)
+    if ext in fileTypes['document_ext']:
+        return fileTypeNames['document']
+    if ext in fileTypes['image_ext']:
+        return fileTypeNames['image']
+    return fileTypeNames['other']
